1e6cf620a1a7687715ff676fc35fcb3d7cd0ee58
Jnino89 <107373653+Jnino89@users.noreply.github.com>
2023-09-18 22:47:53 -0300

    Add files via upload

--- Prueba.py
+++ Prueba.py
@@ -41,7 +41,7 @@ ecg_one_lead = mat_struct['ecg_lead']
 ecg_one_lead = ecg_one_lead.flatten()
 cant_muestras = len(ecg_one_lead)
 
-fs = 300  # Hz
+fs = 1000  # Hz
 nyq_frec = fs / 2
 
 #Plantilla
@@ -52,14 +52,18 @@ atenuacion = 40 # dB
 
 ws1 = 1.0 #Hz
 wp1 = 3.0 #Hz
-wp2 = 25.0 #Hz
-ws2 = 35.0 #Hz
+wp2 = 20.0 #Hz
+ws2 = 30.0 #Hz
 
 frecs = np.array([0.0,         ws1,         wp1,     wp2,     ws2,         nyq_frec   ]) / nyq_frec
 gains = np.array([-atenuacion, -atenuacion, -ripple, -ripple, -atenuacion, -atenuacion])
 gains = 10**(gains/20)
 cant_coeficientes = 1501
 num_win = sig.firwin2(cant_coeficientes, frecs, gains , window='blackmanharris' )
+#num_firls_bp = sig.firls(1200, frecs, gains, fs=fs)
+#num_remez_bp = sig.remez(1200, frecs, gains[::2], fs=fs)
+#num_firls_hp = sig.firls(300, frecs, gains, fs=fs)
+#num_remez_hp = sig.remez(300, frecs, gains[::2], fs=fs)
 #bp_sos_butter, den = sig.iirdesign(wp=np.array([wp1, wp2]) / nyq_frec, ws=np.array([ws1, ws2]) / nyq_frec, gpass=0.5, gstop=40., analog=False, ftype='butter', output='ba')
 den = 1.0
 
@@ -90,4 +94,41 @@ plt.axis([0, 100, -60, 5 ]);
 axes_hdl = plt.gca()
 axes_hdl.legend()
 
-#plot_plantilla(filter_type = 'bandpass', fpass = frecs[[2, 3]]* nyq_frec, ripple = ripple , fstop = frecs[ [1, 4] ]* nyq_frec, attenuation = atenuacion)
+plot_plantilla(filter_type = 'bandpass', fpass = frecs[[2, 3]]* nyq_frec, ripple = ripple , fstop = frecs[ [1, 4] ]* nyq_frec, attenuation = atenuacion)
+# IIR
+# ECG_f_butt = sig.sosfilt(bp_sos_butter, ecg_one_lead)
+
+# FIR
+ECG_f_win = sig.lfilter(num_win, den, ecg_one_lead)
+# demora = int(...)
+# calcule la demora para cada caso, dicha demora adelantará la señal de salida, como puede ver más abajo.
+demora = 750
+
+
+# Segmentos de interés con ALTA contaminación
+
+regs_interes = ( 
+        np.array([5, 5.2]) *60*fs, # minutos a muestras
+        np.array([12, 12.4]) *60*fs, # minutos a muestras
+        np.array([15, 15.2]) *60*fs, # minutos a muestras
+        )
+
+for ii in regs_interes:
+    
+    # intervalo limitado de 0 a cant_muestras
+    zoom_region = np.arange(np.max([0, ii[0]]), np.min([cant_muestras, ii[1]]), dtype='uint')
+    
+    plt.figure(figsize=(fig_sz_x, fig_sz_y), dpi= fig_dpi, facecolor='w', edgecolor='k')
+    plt.plot(zoom_region, ecg_one_lead[zoom_region], label='ECG', linewidth=2)
+    #plt.plot(zoom_region, ECG_f_butt[zoom_region], label='Butter')
+    plt.plot(zoom_region, ECG_f_win[zoom_region + demora], label='Win')
+    
+    plt.title('ECG filtering example from ' + str(ii[0]) + ' to ' + str(ii[1]) )
+    plt.ylabel('Adimensional')
+    plt.xlabel('Muestras (#)')
+    
+    axes_hdl = plt.gca()
+    axes_hdl.legend()
+    axes_hdl.set_yticks(())
+            
+    plt.show()
